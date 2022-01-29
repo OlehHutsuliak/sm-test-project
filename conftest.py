4bc9dc57b6576035c4ec788ce25bebd6c83c4866
OlehHutsuliak <guculoleg24.03@gmail.com>
2022-01-30 00:23:35 +0100

    - New function that return a pdf_file_name ; - Several new browser options.

--- conftest.py
+++ conftest.py
@@ -1,34 +1,56 @@
 import pytest
+import os
 from selenium import webdriver
-from list_of_books import name_urls
+from list_of_books import name_url_pdf
 
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--book", action="store", default=None, help="Write a Book's name"
-    )
-
-
-@pytest.fixture
-def book(request):
-    name = request.config.getoption("--book")
-    if name in name_urls:
-        url = name_urls.get(name)
-        return url
+# PATH for downloaded books
+path = os.path.join(os.getcwd(), "downloaded_book")
 
 
 @pytest.fixture(scope='class')
 def browser():
+    os.mkdir(path)
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    print('\nstart browser for test...')
-    browser = webdriver.Chrome(options=options)
+    options.add_experimental_option('prefs', {
+        "download.default_directory": f"{path}",  # Change default directory for downloads
+        "download.prompt_for_download": False,  # To auto download the file
+        "download.directory_upgrade": True,
+        "plugins.always_open_pdf_externally": True  # It will not show PDF directly in chrome
+    })
+    print('\n..start browser for test..')
+    browser = webdriver.Chrome(options=options)  # options=options
     yield browser
-    print('\nquit browser...')
+    print('\n..quit browser..')
+    # os.rmdir(path)
     browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--book", action="store", default="Engagement Marketing", help="Write a Book's name"
+    )
 
 
 @pytest.fixture
 def name_of_book(request):
     name = request.config.getoption("--book")
     return name
+
+
+@pytest.fixture
+def book(request):
+    name = request.config.getoption("--book")
+    if name in name_url_pdf:
+        list_of_values = name_url_pdf.get(name)
+        url = list_of_values[0]
+        return url
+
+
+@pytest.fixture
+def pdf_file_name(request):
+    name = request.config.getoption("--book")
+    if name in name_url_pdf:
+        list_of_values = name_url_pdf.get(name)
+        pdf_file_name = list_of_values[1]
+        return pdf_file_name
