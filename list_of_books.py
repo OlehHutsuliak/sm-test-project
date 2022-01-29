dfc04242f7f9a4260c6765ba8d9e0769cde973fe
OlehHutsuliak <guculoleg24.03@gmail.com>
2022-01-30 00:20:19 +0100

    Added a new value - pdf_file_name.

--- list_of_books.py
+++ list_of_books.py
@@ -1,38 +1,108 @@
 # Data needed for test
-name_urls = {
-    "Comprehensive guide to NFT current use and future perspectives": "https://salesmanago.com/info/comprehensive-guide-to-nft-current-use-and-future-perspectives.htm",
-    "The Ultimate Guide to Headless Commerce": "https://www.salesmanago.com/info/the-ultimate-guide-to-headless-commerce.htm",
-    "Data Ethics & Customer Preference Management": "https://www.salesmanago.com/info/data-ethics-preference-management.htm",
-    "The Ultimate marketer's guide to Customer Data Platforms": "https://www.salesmanago.com/info/cdp-vs-crm.htm",
-    "AI Product Recommendation Engines: Salesmanago vs Benchmarks": "https://www.salesmanago.com/info/ai-product-recommendation-engines.htm",
-    "Cookbook of marketing automation in eCommerce": "https://www.salesmanago.com/info/cookbok-of-marketing-automation-in-ecommerce.htm",
-    "Kamasutra of eMail Marketing Deliverability": "https://www.salesmanago.com/info/definitive-guide-to-email-deliverability.htm",
-    "The Complete Guide to Marketing Automation in Ecommerce": "https://www.salesmanago.com/info/complete-guide-to-ma-in-ecommerce.htm",
-    "Online Consumer Trends 2020": "https://www.salesmanago.com/info/online_consumer_trends.htm",
-    "How marketing Automation is transformed by AI and Data Science": "https://www.salesmanago.com/info/how_marketing_automation_is_transformed_by_ai_and_data_science.htm",
-    "How Live Chat is redefined by AI, Machine Learning, Deep Learning and Bots": "https://salesmanago.com/info/ebook_live_chat.htm",
-    "10 success stories of Europe's biggest brands implementing marketing automation": "https://www.salesmanago.com/info/10_success_stories_ebook.htm",
-    "Complete Marketing Automation Product Profile": "https://www.salesmanago.com/info/product-profile-salesmanago-new-knowledge.htm",
-    "2018 European Digital Marketing Survey": "https://www.salesmanago.com/info/2018-european-digital-marketing-survey.htm",
-    "The Complete GDPR Guidebook": "https://www.salesmanago.com/info/complete_gdpr_guidebook.htm",
-    "SALESmanago Unique Selling Proposition": "https://www.salesmanago.com/info/unique_selling_proposition.htm",
-    "12 ideas for Omnichannel Marketing": "https://www.salesmanago.com/info/12ideas_for_omnichannel_marketing.htm",
-    "Marketing Automation in Ecommerce for beginners": "https://www.salesmanago.com/info/ma_in_ecommerce_for_beginners.htm",
-    "Social Media Guide for Ecommerce": "https://www.salesmanago.com/info/socialmedia_ecommerce.htm",
-    "5 Ecommerce problems and how to solve them with Marketing Automation Platform Part 2": "https://www.salesmanago.com/info/5_ecommerce_problems_2.htm",
-    "5 Ecommerce problems and how to solve them with Marketing Automation Platform Part 1": "https://www.salesmanago.com/info/5_ecommerce_problems.htm",
-    "The Greate Book of Generation Leads for Ecommerce": "https://www.salesmanago.com/info/great_book_of_generating_leads_ebook.htm",
-    "The Ultimate Guide to Push Notifications": "https://www.salesmanago.com/info/ultimate_guide_to_push_notifications.htm",
-    "Customer Value Marketing": "https://www.salesmanago.com/info/customer_value_marketing_new_knowledge.htm",
-    "Digital Marketer's Pocket Dictionary": "https://www.salesmanago.com/info/pocket_dictionary_new_knowledge.htm",
-    "The Crisis of Inbound and the new Push Marketing": "https://www.salesmanago.com/info/crisis_of_inbound_new_knowledge.htm",
-    "Winning the Zero Moment of Truth with Marketing Automation": "https://www.salesmanago.com/info/zmot_EN_new_knowledge.htm",
-    "Switch from Email Marketing to Marketing Automation": "https://www.salesmanago.com/info/switch_from_email_marketing.htm",
-    "Turn Visitors into Loyal Customers": "https://www.salesmanago.com/info/Turn_visitors_new_knowledge.htm",
-    "Mobile Marketing Automation": "https://www.salesmanago.com/info/mobile_marketing_new_knowledge.htm",
-    "How To Create Amazing Shopping Experience with Marketing Automation": "https://www.salesmanago.com/info/amazing_shopping_experience_new_knowledge.htm",
-    "Buyer Persona in Marketing Automation": "https://www.salesmanago.com/info/persona_new_knowledge.htm",
-    "Internet of Things for marketers": "https://www.salesmanago.com/info/iot_new_knowledge.htm",
-    "The Ultimate Guide to Marketing Automation": "https://www.salesmanago.com/info/definitve_and_ultimate_new_knowledge.htm",
-    "Engagement Marketing": "https://www.salesmanago.com/info/Engagement_Marketing_new_knowledge.htm",
+name_url_pdf = {
+    "Comprehensive guide to NFT current use and future perspectives":
+    ["https://salesmanago.com/info/comprehensive-guide-to-nft-current-use-and-future-perspectives.htm",
+     "NFTguide-ENG.pdf"],
+    "The Ultimate Guide to Headless Commerce":
+    ["https://www.salesmanago.com/info/the-ultimate-guide-to-headless-commerce.htm",
+     "Headless-Commerce-Explained-en-.pdf"],
+    "Data Ethics & Customer Preference Management":
+    ["https://www.salesmanago.com/info/data-ethics-preference-management.htm",
+     "Data-Ethics-en-n1+(4).pdf"],
+    "The Ultimate marketer's guide to Customer Data Platforms":
+    ["https://www.salesmanago.com/info/cdp-vs-crm.htm",
+     "cdp-Ultimate-guide-en.pdf"],
+    "AI Product Recommendation Engines: Salesmanago vs Benchmarks":
+    ["https://www.salesmanago.com/info/ai-product-recommendation-engines.htm",
+     "effectiveness-AI-en.pdf"],
+    "Cookbook of marketing automation in eCommerce":
+    ["https://www.salesmanago.com/info/cookbok-of-marketing-automation-in-ecommerce.htm",
+     "cookbook-reguly-vital-en.pdf"],
+    "Kamasutra of eMail Marketing Deliverability":
+    ["https://www.salesmanago.com/info/definitive-guide-to-email-deliverability.htm",
+     "Definitive_guide_to_email_deliverability_kamasutra_en.pdf"],
+    "The Complete Guide to Marketing Automation in Ecommerce":
+    ["https://www.salesmanago.com/info/complete-guide-to-ma-in-ecommerce.htm",
+     "Complete_Guide_to_MA_in_ecommerce.pdf"],
+    "Online Consumer Trends 2020":
+    ["https://www.salesmanago.com/info/online_consumer_trends.htm",
+     "Online+Consumer+Trends+2020.pdf"],
+    "How marketing Automation is transformed by AI and Data Science":
+    ["https://www.salesmanago.com/info/how_marketing_automation_is_transformed_by_ai_and_data_science.htm",
+     "how_marketing_automation_is_transformed_by_ai_and_data_science.pdf"],
+    "How Live Chat is redefined by AI, Machine Learning, Deep Learning and Bots":
+    ["https://salesmanago.com/info/ebook_live_chat.htm",
+     "Ebook_Live_Chat.pdf"],
+    "10 success stories of Europe's biggest brands implementing marketing automation":
+    ["https://www.salesmanago.com/info/10_success_stories_ebook.htm",
+     "10-success-stories -ENG.pdf"],
+    "Complete Marketing Automation Product Profile":
+    ["https://www.salesmanago.com/info/product-profile-salesmanago-new-knowledge.htm",
+     "SALESmanago_Product_Profile.pdf"],
+    "2018 European Digital Marketing Survey":
+    ["https://www.salesmanago.com/info/2018-european-digital-marketing-survey.htm",
+     "survey_report_2018.pdf"],
+    "The Complete GDPR Guidebook":
+    ["https://www.salesmanago.com/info/complete_gdpr_guidebook.htm",
+     "Complete_GDPR_Guidebook.pdf"],
+    "SALESmanago Unique Selling Proposition":
+    ["https://www.salesmanago.com/info/unique_selling_proposition.htm",
+     "usp_en.pdf"],
+    "12 ideas for Omnichannel Marketing":
+    ["https://www.salesmanago.com/info/12ideas_for_omnichannel_marketing.htm",
+     "12ideas_for_omnichannel.pdf"],
+    "Marketing Automation in Ecommerce for beginners":
+    ["https://www.salesmanago.com/info/ma_in_ecommerce_for_beginners.htm",
+     "ma_in_ecommerce_for_beginners.pdf"],
+    "Social Media Guide for Ecommerce":
+    ["https://www.salesmanago.com/info/socialmedia_ecommerce.htm",
+     "social_media_for_ecommerce.pdf"],
+    "5 Ecommerce problems and how to solve them with Marketing Automation Platform Part 2":
+    ["https://www.salesmanago.com/info/5_ecommerce_problems_2.htm",
+     "5ecommerce_problems_part2.pdf"],
+    "5 Ecommerce problems and how to solve them with Marketing Automation Platform Part 1":
+    ["https://www.salesmanago.com/info/5_ecommerce_problems.htm",
+     "5ecommerce_problems.pdf"],
+    "The Greate Book of Generation Leads for Ecommerce":
+    ["https://www.salesmanago.com/info/great_book_of_generating_leads_ebook.htm",
+     "the_great_book_of_generating_leads.pdf"],
+    "The Ultimate Guide to Push Notifications":
+    ["https://www.salesmanago.com/info/ultimate_guide_to_push_notifications.htm",
+     "push_notifications.pdf"],
+    "Customer Value Marketing":
+    ["https://www.salesmanago.com/info/customer_value_marketing_new_knowledge.htm",
+     "Customer_Value_Marketing.pdf"],
+    "Digital Marketer's Pocket Dictionary":
+    ["https://www.salesmanago.com/info/pocket_dictionary_new_knowledge.htm",
+     "pocket_dictionary.pdf"],
+    "The Crisis of Inbound and the new Push Marketing":
+    ["https://www.salesmanago.com/info/crisis_of_inbound_new_knowledge.htm",
+     "The_Crisis_Of_Inbound.pdf"],
+    "Winning the Zero Moment of Truth with Marketing Automation":
+    ["https://www.salesmanago.com/info/zmot_EN_new_knowledge.htm",
+     "Winning_the_Zero_Moment_of_Truth_with_Marketing_Automation.pdf"],
+    "Switch from Email Marketing to Marketing Automation":
+    ["https://www.salesmanago.com/info/switch_from_email_marketing.htm",
+     "Switch_From_Email_Marketing.pdf"],
+    "Turn Visitors into Loyal Customers":
+    ["https://www.salesmanago.com/info/Turn_visitors_new_knowledge.htm",
+     "zmien_odbiorcow_en.pdf"],
+    "Mobile Marketing Automation":
+    ["https://www.salesmanago.com/info/mobile_marketing_new_knowledge.htm",
+     "mobile_marketing_ebookEN.pdf"],
+    "How To Create Amazing Shopping Experience with Marketing Automation":
+    ["https://www.salesmanago.com/info/amazing_shopping_experience_new_knowledge.htm",
+     "niezwykle_doswiad_zakupowe_EN.pdf"],
+    "Buyer Persona in Marketing Automation":
+    ["https://www.salesmanago.com/info/persona_new_knowledge.htm",
+     "buyerpersonaEN.pdf"],
+    "Internet of Things for marketers":
+    ["https://www.salesmanago.com/info/iot_new_knowledge.htm",
+     "internet_of_things_salesmanago_en.pdf"],
+    "The Ultimate Guide to Marketing Automation":
+    ["https://www.salesmanago.com/info/definitve_and_ultimate_new_knowledge.htm",
+     "The_Definitive_and_Ultimate_Guide_to_Marketing_Automation.pdf"],
+    "Engagement Marketing":
+    ["https://www.salesmanago.com/info/Engagement_Marketing_new_knowledge.htm",
+     "eBook_Engagement_Marketing.pdf"]
 }
